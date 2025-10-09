abef77c0c9cce3e07f4c5e7db0188ad8b02aaad7
maakhady <makhadypro@gmail.com>
2025-10-09 18:02:38 +0000

    FIX: Generate and commit the correct initial migration derniere

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,9 +1,12 @@
 import logging
 from logging.config import fileConfig
+from sqlalchemy import engine_from_config
 
 from flask import current_app
 
 from alembic import context
+
+from app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
